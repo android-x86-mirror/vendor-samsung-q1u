f1d773b687d407b919c5d222a108e5c3eded6c61
Bryan Hundven <bryanhundven@gmail.com>
2009-12-06 16:46:25 -0800

    Fix installation of /data/system/tslib/pointercal

--- ins_pointercal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -f /system/etc/pointercal ]; then
-	mkdir /data/system/tslib
-	chown 1000.1000 /data/system/tslib
-	cp /system/etc/pointercal /data/system/tslib/pointercal
-	chown 1000.1000 /data/system/tslib/pointercal
-fi
