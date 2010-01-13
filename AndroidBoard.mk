f448078b35ba2ec4b5de55d2fa00e2f1dca51f2c
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-01-13 16:10:27 +0800

    remove copy of pointercal ts.conf ts.env
    The rules are now moved to generic_x86.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,6 +3,4 @@ LOCAL_FIRMWARES :=
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/q1u_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 
-$(call add-prebuilt-targets,$(TARGET_OUT_DATA_ETC),pointercal ts.conf ts.env)
-
 include $(GENERIC_X86_ANDROID_MK)
